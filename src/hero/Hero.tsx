da1941be86b12ba9442af683f463c590482393e9
EnzyZero <jpetracek@t-online.de>
2025-09-04 11:34:57 +0200

    Added profile picture

--- src/hero/Hero.tsx
+++ src/hero/Hero.tsx
@@ -1,8 +1,50 @@
+import ProfileJPG from '../assets/me.jpg'
 import './hero.css'
 
 interface Props {
   active: boolean;
 }
+
+const heroStruct = 
+<div className="hero-struct">
+  <span className="hero-struct-braces">{"{"}</span>
+
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">name</span>
+    :
+    <span className="hero-struct-value">jakob</span>
+  </div>
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">surname</span>
+    :
+    <span className="hero-struct-value">petracek</span>
+  </div>
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">exp</span>
+    :
+    <span className="hero-struct-value">junior_dev</span>
+  </div>
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">domain</span>
+    :
+    <span className="hero-struct-value">petracek.me</span>
+  </div>
+
+  <div className="hero-struct-pair-spacer"></div>
+
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">employed</span>
+    :
+    <span className="hero-struct-value false">false</span>
+  </div>
+  <div className="hero-struct-pair">
+    <span className="hero-struct-key">cs_student</span>
+    :
+    <span className="hero-struct-value true">true</span>
+  </div>
+
+  <span className="hero-struct-braces">{"}"}</span>
+</div>;
 
 export default function Hero({ active }: Props) {
 
@@ -13,46 +55,15 @@ export default function Hero({ active }: Props) {
       <h1 className="hero-title">
         Hi, I'm a 
         <span className="hero-title-animation"> Developer</span>
+        !
       </h1>
-      <div className="hero-struct">
-        <span className="hero-struct-braces">{"{"}</span>
 
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">name</span>
-          :
-          <span className="hero-struct-value">jakob</span>
-        </div>
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">surname</span>
-          :
-          <span className="hero-struct-value">petracek</span>
-        </div>
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">exp</span>
-          :
-          <span className="hero-struct-value">junior_dev</span>
-        </div>
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">domain</span>
-          :
-          <span className="hero-struct-value">petracek.me</span>
-        </div>
 
-        <div className="hero-struct-pair-spacer"></div>
-
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">employed</span>
-          :
-          <span className="hero-struct-value false">false</span>
-        </div>
-        <div className="hero-struct-pair">
-          <span className="hero-struct-key">cs_student</span>
-          :
-          <span className="hero-struct-value true">true</span>
-        </div>
-
-        <span className="hero-struct-braces">{"}"}</span>
+      <div className="hero-profile-container">
+        <img src={ProfileJPG} alt="Jakob Petracek" className="hero-profile-picture desktop" />
+        {heroStruct}
       </div>
+
       <div className="hero-buttons">
         <a href="#about">
           <button>About</button>
@@ -67,6 +78,9 @@ export default function Hero({ active }: Props) {
           <button>Github</button>
         </a>
       </div>
+
+      <img src={ProfileJPG} alt="Jakob Petracek" className="hero-profile-picture mobile" />
+
     </div>
   );
 }
